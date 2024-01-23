72bf9f03e91c10408d45ae0daa7ef64a0f66b171
Tejas Gadi <tejasgadi@Tejass-MacBook-Pro.local>
2024-01-24 01:31:27 +0530

    Added Data Augmentation step and also trained model on this data and saved weights

--- fast_api/app/main.py
+++ fast_api/app/main.py
@@ -4,40 +4,52 @@ from fastapi.responses import JSONResponse
 from PIL import Image
 import io
 import numpy as np
+import tensorflow as tf
 
 app = FastAPI()
 
 # Assuming you've loaded your image model somewhere
 image_model = load_image_model()
 
-# Mocking a simple image model for demonstration
-def mock_image_model_predict(image_array):
-    # Replace this with your actual prediction logic
-    return np.random.rand(1, 12)  # Assuming 12 classes
-
 @app.post("/classify")
 async def classify_image(file: UploadFile = File(...)):
     try:
+        
         # Read image file
         contents = await file.read()
+        # Save the image file temporarily
+        # with open(file.filename, "wb") as image_file:
+        #     image_file.write(contents)
         
         # Preprocess the image data
-        image = Image.open(io.BytesIO(contents)).convert("RGB")
-        image_array = np.array(image.resize((224, 224))) / 255.0
-        image_array = np.expand_dims(image_array, axis=0)
+        img = tf.keras.preprocessing.image.load_img(io.BytesIO(contents), target_size=(224, 224))
+        img_array = tf.keras.preprocessing.image.img_to_array(img)
+        img_array = np.array([img_array])
 
         # Make predictions using the loaded model
-        predictions = image_model.predict(image_array)
+        predictions = image_model.predict(img_array)
 
 
         class_id = np.argmax(predictions, axis=1).item()
-        class_names = ["battery", "biological", "brown glass", "clothes", "green glass", "metal", "paper", "plastic", "shoes",
-                       "trash", "white glass", "cardboard"]
-        # Example code for extracting class probabilities and class name
+        # define classes name
+        class_names = [ "battery",
+                        "biological",
+                        "brown-glass",
+                        "cardboard",
+                        "clothes",
+                        "green-glass",
+                        "metal",
+                        "paper",
+                        "plastic",
+                        "shoes",
+                        "trash",
+                        "white-glass"]
+
+        # Extracting class probabilities and class name
         class_probabilities = predictions.tolist()[0]
         class_name = class_names[class_id]
 
-        return JSONResponse(content={"class_probabilities": class_probabilities, "class": class_id})
+        return JSONResponse(content={"class_probabilities": class_probabilities,"class": class_name})
 
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
--- saved_model/my_model/keras_metadata.pb
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3d91ac44f48112d2721078ab5c1933803ed1091a7236860edccdb1468e814191
-size 395831
